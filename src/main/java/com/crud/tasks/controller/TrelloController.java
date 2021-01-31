18f57b62de0ad22d79693d0064224259d29e8f15
maciek047 <maciek.047@gmail.com>
2021-02-01 00:45:38 +0100

    mail added v2

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -13,7 +13,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.Locale;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/trello")
 public class TrelloController {
